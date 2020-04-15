4cc6ed3be43254c862b5671d6c77204763aba595
Will-Harris00 <57931473+Will-Harris00@users.noreply.github.com>
2020-04-16 00:07:46 +0100

    Delete handler.inc.php

--- src/assets/db/inc/handler.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-require "../../secure/credentials.php";
-$conn = mysqli_connect(host, user, password, database, port);
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error() . PHP_EOL);
-}
-?>
