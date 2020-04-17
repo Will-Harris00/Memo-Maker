85f1c4a268f7f1c9719c8dc36cea4da4f7071760
William Harris <wjharris00@outlook.com>
2020-04-17 23:10:25 +0100

    styling backed up

--- assets/css/getstyle.php
+++ assets/css/getstyle.php
@@ -46,68 +46,167 @@ General styling
 
 
 html {
-    height: 100%;
-    margin: 0;
-    padding: 0;
-    font-family: 'Roboto', sans-serif;
-    color: #292929;
+height: 100%;
+margin: 0;
+padding: 0;
+font-family: 'Roboto', sans-serif;
+color: #292929;
 }
 
 body {
-    height: 100%;
-    margin: 0;
-    background-repeat: no-repeat;
-    background-attachment: fixed;
-    background-position: center;
-    background-size: cover;
-    font-size: 1rem;
-    display: flex;
-    flex-direction: column;
-    background-color: <?php echo $bg; ?>;
+height: 100%;
+margin: 0;
+background-repeat: no-repeat;
+background-attachment: fixed;
+background-position: center;
+background-size: cover;
+font-size: 1rem;
+display: flex;
+flex-direction: column;
+background-color: <?php echo $bg ?>;
 }
 
 main {
-    max-width: 800px;
-    margin: auto;
-    padding: 0.5rem;
-    text-align: center;
+max-width: 800px;
+margin: auto;
+padding: 0.5rem;
+text-align: center;
 }
 
-td {
-    text-align:center;
+table, td, th {
+padding:5px;
+border: 1px solid PowderBlue;
+font-weight: normal;
+text-align: center;
+}
+
+.desc_scroll {
+max-height:175px;
+overflow-x: hidden;
+overflow-y: auto;
+padding-left: 2px;
+padding-right: 2px;
+}
+
+td#desc_scroll {
+padding: 2px;
+/* keeps paragraph formatting and newline breaks */
+white-space: pre-wrap;
+/* allows for splitting of continuous strings with no whitespaces */
+word-wrap: break-word;
+word-break: break-word;
+/*page-break-inside: auto;*/
+overflow-x: hidden;
+overflow-y: auto;
+width: auto;
+max-width: 600px;
+}
+
+.name_scroll {
+max-height:175px;
+overflow-x: hidden;
+overflow-y: auto;
+padding-left: 2px;
+padding-right: 2px;
+}
+
+td#name_scroll {
+padding: 2px;
+/* keeps paragraph formatting and newline breaks */
+white-space: pre-wrap;
+/* allows for splitting of continuous strings with no whitespaces */
+word-wrap: break-word;
+word-break: break-word;
+/*page-break-inside: auto;*/
+overflow-x: hidden;
+overflow-y: auto;
+width: auto;
+max-width: 125px;
+}
+
+th {
+color: white;
+cursor: pointer;
+background-color: rgba(29,150,178,1);
+border: 1px solid rgba(29,150,178,1);
+}
+
+tr:nth-child(odd) {
+background-color: #ffffff;
+}
+
+tr:nth-child(even) {
+background-color: #f2f2f2;
+}
+
+table tr:not(:first-child) {
+cursor: pointer;transition: all .25s ease-in-out;
+}
+
+table tr:not(:first-child):hover {
+background-color: #ddd;
+}
+
+table {
+border-collapse: collapse;
+width:100%;
+}
+
+div.container {
+position: relative;
+width: 100%;
+}
+
+div.table {
+position: static;
+width: 100%;
+top:0;
+left:0;
+}
+
+div.edit {
+background-color: white;
+position: absolute;
+width:100%;
+height: 100%;
+top:0;
+left:0;
+z-index: 1;
+visibility: hidden;
 }
 
 textarea {
-    border: 1px solid transparent;
-    padding: 10px;
-    font-size: 16px;
-    background-color: White;
-    width: 100%;
-    resize: vertical;
-    min-height: 150px;
+font-family: 'Roboto', sans-serif;
+border: 1px solid transparent;
+padding: 10px;
+font-size: 16px;
+background-color: White;
+width: 100%;
+resize: vertical;
+min-height: 150px;
 }
 
 input {
-    border: 1px solid transparent;
-    background-color: White;
-    padding: 10px;
-    font-size: 16px;
-    width: 100%;
+border: 1px solid transparent;
+background-color: White;
+padding: 10px;
+font-size: 16px;
+width: 100%;
 }
 
 input[type=submit], button {
-    border: 1px solid transparent;
-    background-color: DodgerBlue;
-    color: white;
-    cursor: pointer;
-    padding: 10px;
-    width: auto;
+border: 1px solid transparent;
+background-color: DodgerBlue;
+color: white;
+cursor: pointer;
+padding: 10px;
+width: auto;
 }
 
 input[type=password]:focus {
-    outline:none;
-    border-color:DodgerBlue;
-    box-shadow:0 0 10px DodgerBlue;
+outline:none;
+border-color:DodgerBlue;
+box-shadow:0 0 10px DodgerBlue;
 }
 
 
@@ -119,40 +218,40 @@ Navigation Bar
 
 
 nav ul {
-    font-family: 'Lato', sans-serif;
-    width: 100%;
-    padding: 0;
-    margin: 0;
-    top: 0;
-    background-color: <?php echo $fg; ?>;
+font-family: 'Lato', sans-serif;
+width: 100%;
+padding: 0;
+margin: 0;
+top: 0;
+background-color: <?php echo $fg ?>;
 }
 nav ul li, a {
-    display: inline-block;
-    text-align: center;
+display: inline-block;
+text-align: center;
 }
 
 nav ul li a::before {
-    display: block;
-    content: attr(id);
-    font-weight: bold;
-    height: 0;
-    overflow: hidden;
-    visibility: hidden;
+display: block;
+content: attr(id);
+font-weight: bold;
+height: 0;
+overflow: hidden;
+visibility: hidden;
 }
 
 nav ul li a {
-    display: inline-block;
-    padding: 20px 15px;
-    text-align: center;
-    margin: auto;
-    text-decoration: none;
-    color: DarkSlateGray;
+display: inline-block;
+padding: 20px 15px;
+text-align: center;
+margin: auto;
+text-decoration: none;
+color: DarkSlateGray;
 }
 
 nav ul li a:hover {
-    font-weight: bold;
-    animation: bounce 0.3s;
-    color: CadetBlue;
+font-weight: bold;
+animation: bounce 0.3s;
+color: CadetBlue;
 }
 
 /*
@@ -161,7 +260,7 @@ element on the page but for now I am sticking to only making changes to
 background and foreground colours, as such this hover effect is redundant.
 
 nav ul li:hover{
-    background: Honeydew;
+background: Honeydew;
 }
 */
 
@@ -174,45 +273,45 @@ Preferences selector
 
 
 * {
-    box-sizing: border-box;
-    padding: 0;
+box-sizing: border-box;
+padding: 0;
 }
 
 /*the container must be positioned relative:*/
-    .autocomplete {
-    width: 300px;
-    position: relative;
-    display: inline-block;
+.autocomplete {
+width: 300px;
+position: relative;
+display: inline-block;
 }
 
 .autocomplete-items {
-    position: absolute;
-    border: 1px solid #d4d4d4;
-    border-bottom: none;
-    border-top: none;
-    z-index: 1;
-    /*position the autocomplete items to be the same width as the container:*/
-    top: 100%;
-    left: 0;
-    right: 0;
+position: absolute;
+border: 1px solid #d4d4d4;
+border-bottom: none;
+border-top: none;
+z-index: 1;
+/*position the autocomplete items to be the same width as the container:*/
+top: 100%;
+left: 0;
+right: 0;
 }
 
 .autocomplete-items div {
-    padding: 10px;
-    cursor: pointer;
-    background-color: #fff;
-    border-bottom: 1px solid #d4d4d4;
+padding: 10px;
+cursor: pointer;
+background-color: #fff;
+border-bottom: 1px solid #d4d4d4;
 }
 
 /*when hovering an item:*/
 .autocomplete-items div:hover {
-    background-color: #e9e9e9;
+background-color: #e9e9e9;
 }
 
 /*when navigating through the items using the arrow keys:*/
 .autocomplete-active {
-    background-color: DodgerBlue !important;
-    color: #ffffff;
+background-color: DodgerBlue !important;
+color: #ffffff;
 }
 
 
@@ -226,48 +325,48 @@ Animations
 /* Safari 4.0 - 8.0 */
 
 @-webkit-keyframes bounce {
-    0% {
-        transform: translate(0px, 1px);
-    }
-    20% {
-        transform: translate(0px, 2px);
-    }
-    40% {
-        transform: translate(0px, -2px);
-    }
-    60% {
-        transform: translate(0px, -2px);
-    }
-    80% {
-        transform: translate(0px, 2px);
-    }
-    100% {
-        transform: translate(0px, 3px);
-    }
+0% {
+transform: translate(0px, 1px);
+}
+20% {
+transform: translate(0px, 2px);
+}
+40% {
+transform: translate(0px, -2px);
+}
+60% {
+transform: translate(0px, -2px);
+}
+80% {
+transform: translate(0px, 2px);
+}
+100% {
+transform: translate(0px, 3px);
+}
 }
 
 
 /* Others */
 
 @keyframes bounce {
-    0% {
-        transform: translate(0px, 1px);
-    }
-    20% {
-        transform: translate(0px, 2px);
-    }
-    40% {
-        transform: translate(0px, -2px);
-    }
-    60% {
-        transform: translate(0px, -2px);
-    }
-    80% {
-        transform: translate(0px, 2px);
-    }
-    100% {
-        transform: translate(0px, 3px);
-    }
+0% {
+transform: translate(0px, 1px);
+}
+20% {
+transform: translate(0px, 2px);
+}
+40% {
+transform: translate(0px, -2px);
+}
+60% {
+transform: translate(0px, -2px);
+}
+80% {
+transform: translate(0px, 2px);
+}
+100% {
+transform: translate(0px, 3px);
+}
 }
 
 
@@ -279,8 +378,52 @@ Styling for small screens
 
 
 @media screen and (max-width: 700px) {
-    nav ul li, a {
-        display: block;
-        text-align: center;
-    }
+nav ul li, a {
+display: block;
+text-align: center;
+}
+
+.desc_scroll {
+max-height:168px;
+overflow-x: hidden;
+overflow-y: auto;
+padding-left: 2px;
+padding-right: 2px;
+}
+
+td#desc_scroll {
+padding: 2px;
+/* keeps paragraph formatting and newline breaks */
+white-space: pre-wrap;
+/* allows for splitting of continuous strings with no whitespaces */
+word-wrap: break-word;
+word-break: break-word;
+/*page-break-inside: auto;*/
+overflow-x: hidden;
+overflow-y: auto;
+width: auto;
+max-width: 500px;
+}
+
+.name_scroll {
+max-height:168px;
+overflow-x: hidden;
+overflow-y: auto;
+padding-left: 2px;
+padding-right: 2px;
+}
+
+td#name_scroll {
+padding: 2px;
+/* keeps paragraph formatting and newline breaks */
+white-space: pre-wrap;
+/* allows for splitting of continuous strings with no whitespaces */
+word-wrap: break-word;
+word-break: break-word;
+/*page-break-inside: auto;*/
+overflow-x: hidden;
+overflow-y: auto;
+width: auto;
+max-width: 225px;
+}
 }
