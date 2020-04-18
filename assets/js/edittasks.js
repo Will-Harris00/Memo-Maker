3de1f68248495597ac0492137c53d270d6730e2f
William Harris <wjharris00@outlook.com>
2020-04-18 13:50:22 +0100

    Tables text replacement complete

--- assets/js/edittasks.js
+++ assets/js/edittasks.js
@@ -8,10 +8,9 @@ for(var i = 1; i < table.rows.length; i++)
     table.rows[i].onclick = function()
     {
         rIndex = this.rowIndex;
-        console.log(rIndex);
-
-        document.getElementById("name").value = this.cells[0].innerHTML;
-        document.getElementById("description").value = this.cells[1].innerHTML;
+        console.log(rIndex - 1);
+        document.getElementById("name").value = table.getElementsByClassName("name_scroll")[rIndex - 1].innerHTML;
+        document.getElementById("description").value = table.getElementsByClassName("desc_scroll")[rIndex - 1].innerHTML;
         document.getElementById("due").value = this.cells[2].innerHTML;
         document.getElementById("state").value = this.cells[3].innerHTML;
         document.getElementById("edit").style.visibility = "visible";
@@ -22,8 +21,8 @@ for(var i = 1; i < table.rows.length; i++)
 // edit the row
 function editRow()
 {
-    table.rows[rIndex].cells[0].innerHTML = document.getElementById("name").value;
-    table.rows[rIndex].cells[1].innerHTML = document.getElementById("description").value;
+    table.getElementsByClassName("name_scroll")[rIndex - 1].innerHTML = document.getElementById("name").value;
+    table.getElementsByClassName("desc_scroll")[rIndex - 1].innerHTML = document.getElementById("description").value;
     table.rows[rIndex].cells[2].innerHTML = document.getElementById("due").value;
     table.rows[rIndex].cells[3].innerHTML = document.getElementById("state").value;
     document.getElementById("edit").style.visibility = "hidden";
