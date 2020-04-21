5ddc815ce996cb4ee84f64974e796e018a1bc7cd
William Harris <wjharris00@outlook.com>
2020-04-21 17:59:01 +0100

    counting number of selected checkboxes

--- assets/db/import-tasks.php
+++ assets/db/import-tasks.php
@@ -4,7 +4,7 @@ require "header.php";
 
 $xml = new SimpleXMLElement($_SESSION['imports']);
 
-echo "<form action='inc/add-imports.inc.php' method='post'>
+echo "<form action='inc/add-imports.inc.php' method='post' onsubmit='return confirm(\"Are you sure you want to import \" + countChecked() + \"tasks?\")'>
           <input type='submit' name='import_tasks' id='import_tasks' value='Import'>
           <table> 
               <tr>
@@ -20,7 +20,8 @@ foreach($xml->task as $task) {
 echo "    </table>
       </form>
       <a id=\"back2Top\" title=\"Back to top\" href=\"#\">➤</a>
-      <script src=\"../js/scroll-arrow.js\"></script>";
+      <script src=\"../js/scroll-arrow.js\"></script>
+      <script>function countChecked() {return document.querySelectorAll('input[type=\"checkbox\"]:checked').length;}</script>";
 
 require "footer.php";
 ?>
