f15119563d3134fcfa8b87b197f5e7c7dbb68822
William Harris <wjharris00@outlook.com>
2020-04-28 07:26:52 +0100

    fixed syncing and change of state for remote tasks.

--- assets/db/inc/update-state.inc.php
+++ assets/db/inc/update-state.inc.php
@@ -1,26 +1,42 @@
 <?php
-// This script handles change in state for remotely synced tasks.
+// This script handles change in state for synchronised remote tasks.
 session_start();
 if (!(isset($_SESSION['userid']))) {
     header("Location: ../login.php");
     exit();
 }
 
-if (isset($_SESSION['tasks'])) {
+if (isset($_POST['importid'])) {
     $userid = $_SESSION['userid'];
-    $tasks = $_SESSION['tasks'];
+    $importid = $_POST['importid'];
+    $state = $_POST['state'];
     require "handler.inc.php";
 
-    // The & symbol signifies that we are passing by reference to an item in the array.
-    foreach($tasks as &$task) {
-        $importid = $task['importid'];
-        $taskid = $task['taskid'];
+    $user = new SimpleXMLElement('<user/>');
+    $user->addChild('id', $userid);
+    $user = $user->asXML();
 
-        $user = new SimpleXMLElement('<user/>');
-        $user->addChild('id', $userid);
-        $user = $user->asXML();
+    $url = "http://students.emps.ex.ac.uk/dm656/check.php/" . $importid;
 
-        $url = "http://students.emps.ex.ac.uk/dm656/check.php/" . $importid;
+    $curl = curl_init($url);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, array("Content-Type: text/xml"));
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'PUT');
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $user);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 300);
+    $result = curl_exec($curl);
+    $response = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+    curl_close($curl);
+
+    if ($response == 200) {
+        echo "200 Success - Status updated successfully.";
+        // Task was unchecked
+        $state = 0;
+        /* Because the response code was retrieved using the check method
+           we need to ensure that the task is once again left unchecked. */
+
+        $url = "http://students.emps.ex.ac.uk/dm656/uncheck.php/" . $importid;
 
         $curl = curl_init($url);
         curl_setopt($curl, CURLOPT_HTTPHEADER, array("Content-Type: text/xml"));
@@ -30,41 +46,29 @@ if (isset($_SESSION['tasks'])) {
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 300);
         $result = curl_exec($curl);
-        $response = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        $other_response = curl_getinfo($curl, CURLINFO_HTTP_CODE);
         curl_close($curl);
-
-        if ($response == 200) {
-            unset($_SESSION['response']);
-            $_SESSION['response'] = $response;
-            // Task was unchecked
-            $state = 0;
-            /* Because the response code was retrieved using the check method
-               we need to ensure that the task is once again left unchecked. */
-
-            $url = "http://students.emps.ex.ac.uk/dm656/uncheck.php/" . $importid;
-
-            $curl = curl_init($url);
-            curl_setopt($curl, CURLOPT_HTTPHEADER, array("Content-Type: text/xml"));
-            curl_setopt($curl, CURLOPT_URL, $url);
-            curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'PUT');
-            curl_setopt($curl, CURLOPT_POSTFIELDS, $user);
-            curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-            curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 300);
-            $result = curl_exec($curl);
-            $other_response = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-            curl_close($curl);
-        } elseif ($response == 409) {
-            // The task was already checked
-            $state = 1;
-            /* We made an attempt to check an event that has already been marked
-               as complete, as a result no changes were made to the web service. */
-        } elseif ($response == 401) {
-            //  User tried to uncheck a task which they did not originally check.
-            $state = 1;
-        }
+    } elseif ($response == 409) {
+        echo "409 Conflict - Tried to mark a task as done which was already marked complete.";
+        // The task was already checked
+        $state = 1;
+        /* We made an attempt to check an event that has already been marked
+           as complete, as a result no changes were made to the web service. */
+    } elseif ($response == 401) {
+        echo "401 Unauthorized - Tried to uncheck a task which the user did not originally check.";
+        // User tried to uncheck a task which they did not originally check.
+        $state = 1;
+    }
+    if (empty($userid) || empty($importid) || !isset($state)) {
+        header("Location: ../view-tasks.php?error=emptyfields&userid=" . $userid);
+        mysqli_close($conn);
+        exit();
+    } else {
+        /* Matches only importid thereby updating the state of the
+        remote task simultaneously for all users on the local site. */
         $sql = "UPDATE Tasks 
-                SET state=? 
-                WHERE taskid=?";
+                SET state = ? 
+                WHERE importid = ?";
 
         $stmt = mysqli_stmt_init($conn);
 
@@ -74,15 +78,12 @@ if (isset($_SESSION['tasks'])) {
             mysqli_close($conn);
             exit();
         } else {
-            mysqli_stmt_bind_param($stmt, "ii", $state, $taskid);
+            mysqli_stmt_bind_param($stmt, "ii", $state, $importid);
             mysqli_stmt_execute($stmt);
             mysqli_stmt_close($stmt);
+            exit();
         }
-        $task['state'] = $state;
     }
-    $_SESSION['tasks'] = $tasks;
-    // header("Location: ../view-tasks.php");
-    exit();
 } else {
     header("Location: view-tasks.inc.php");
     exit();
--- assets/js/change-state.js
+++ assets/js/change-state.js
@@ -2,9 +2,12 @@ function toggleState(importid, taskid, userid) {
         let checkbox = document.getElementById("check-" + taskid.toString());
         if (importid != null) {
                 let url = "http://students.emps.ex.ac.uk/dm656/";
+                state = 0
                 let toggle_url = url + "uncheck.php/";
-                if (checkbox.checked) toggle_url = url + "check.php/";
-
+                if (checkbox.checked) {
+                        state = 1
+                        toggle_url = url + "check.php/";
+                }
                 let request = new XMLHttpRequest();
                 /* With async set to true the user does not have to wait for
                    the web service to handle the query and send a response. */
@@ -12,16 +15,26 @@ function toggleState(importid, taskid, userid) {
                 request.setRequestHeader('Content-Type', 'text/xml');
                 let xml = "<user><id>" + userid.toString() + "</id></user>";
                 request.onload = function () {
-                        window.location.replace("inc/view-tasks.inc.php");
+                        let query = new XMLHttpRequest();
+                        query.open("POST", "inc/update-state.inc.php", true);
+                        query.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+                        query.onreadystatechange = function () {
+                                if (query.readyState === 4 && query.status === 200) {
+                                        alert(query.responseText)
+                                }
+                        };
+                        query.onload = function () {
+                                window.location.replace("inc/view-tasks.inc.php");
+                        };
+                        query.send("importid=" + importid + "&state=" + state);
                 };
                 request.send(xml);
         } else {
                 var state
                 var request = new XMLHttpRequest();
+                state = 0
                 if (checkbox.checked) {
                         state = 1
-                } else {
-                        state = 0
                 }
                 request.open("POST", "inc/change-state.inc.php", true);
                 request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
