e1b5e02f4d8fc0e81d2fdb3367d30da4623401f4
Will-Harris00 <wjharris00@outlook.com>
2021-01-17 13:43:53 +0000

    Redefined DB Handler and Secure Pepper

--- assets/secure/credentials.php
+++ assets/secure/credentials.php
@@ -1,7 +1,7 @@
 <?php
-define(HOST, 'emps-sql.ex.ac.uk');
-define(USER, 'wjph202');
-define(PASSWORD, 'wjph202');
-define(DATABASE, 'wjph202');
-define(PORT, '3306')
+define('HOST', 'emps-sql.ex.ac.uk');
+define('USER', 'wjph202');
+define('PASSWORD', 'wjph202');
+define('DATABASE', 'wjph202');
+define('PORT', '3306')
 ?>
