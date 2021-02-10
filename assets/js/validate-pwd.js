ca0b38c3aca6a2c6d273815d013a844ed6de1917
Will-Harris00 <wjharris00@outlook.com>
2021-02-10 13:02:19 +0000

    Removed Console Log

--- assets/js/validate-pwd.js
+++ assets/js/validate-pwd.js
@@ -1,9 +1,8 @@
-var  timerId;
+var timerId;
 var inputs = document.signup_form;
 var pass = inputs.password;
 var confirm_pass = inputs.confirm_password;
 var signup_btn = inputs.signup_btn;
-console.log(confirm_pass);
 
 // Event listener on the input box
 confirm_pass.addEventListener('input', function() {
